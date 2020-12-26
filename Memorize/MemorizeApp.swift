54fb0e7a0d40e09ca095019622b152ca4aaaa392
Robert Roy <me@robert-e-roy.com>
2020-12-26 15:34:59 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Robert Roy on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
